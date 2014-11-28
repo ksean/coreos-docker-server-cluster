a260ad01249c6198224a86d05fc64045f04d9fc6
Sean Kennedy <kennedymsean@gmail.com>
2014-11-28 01:08:16 -0500

    - Added build and push shortcuts to control script

--- control.sh
+++ control.sh
@@ -20,3 +20,19 @@ if [ "$1" = "jenkins" ] || [ "$1" = "all" ]; then
     fleetctl $2 ./service/etcd/etcd-jenkins.service
     fleetctl $2 ./service/register/reg-jenkins.service
 fi
+
+#Build
+if [ "$1" = "build" ]; then
+    docker build --no-cache -t="ksean/apache" ./docker/apache
+    docker build --no-cache -t="ksean/apache-data" ./docker/apache/volume/data
+    docker build --no-cache -t="ksean/apache-log" ./docker/apache/volume/log
+    docker build --no-cache -t="ksean/haproxy" ./docker/haproxy
+    docker build --no-cache -t="ksean/jenkins" ./docker/jenkins
+    docker build --no-cache -t="ksean/jenkins-log" ./docker/jenkins/volume/log
+    docker push ksean/apache
+    docker push ksean/apache-data
+    docker push ksean/apache-log
+    docker push ksean/haproxy
+    docker push ksean/jenkins
+    docker push ksean/jenkins-log
+fi
